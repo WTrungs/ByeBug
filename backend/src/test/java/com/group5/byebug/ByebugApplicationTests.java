e951269211f4bc33c7539b21be59dae7e3973ef7
WTrungs <godhander274@gmail.com>
2026-04-13 14:08:03 +0700

    feat!: restart project from scratch

---
+++ backend/src/test/java/com/group5/byebug/ByebugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.byebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByebugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
