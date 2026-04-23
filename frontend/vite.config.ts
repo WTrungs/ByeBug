22b2f16538392b465539aa9777f9d00705e69eda
WTrungs <godhander274@gmail.com>
2026-04-23 12:30:06 +0700

    chore: move server port to environment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
+export default defineConfig(({ mode }) => {
+  const envDir = './..'; 
+  const env = loadEnv(mode, envDir, '');
+
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000,
+      strictPort: true,
+    },
+  }
 })
